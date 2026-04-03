6f614b1183a49ff80e70de0dfcf3d3b1919f6d43
Mykola <tesliamain@gmail.com>
2026-04-03 08:46:00 +0300

    chore: apply .gitignore rules

--- envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-lab4-teslia-mykola-20"
-    key            = "serverless-lab4/dev/terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-  }
-}
